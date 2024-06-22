71dfe02eba0ff013b840ac3ec7c572a1a27fc9e6
Taha Alfoghi <102795632+tahaalfoghi@users.noreply.github.com>
2024-06-22 06:56:22 +0200

    Edit Home Page

--- App/obj/Debug/net8.0/App.AssemblyInfo.cs
+++ App/obj/Debug/net8.0/App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4ec902c95324955e5db1ac6413f37e8262809a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40f5986188f067721e0a4b78070f967eab5a9e2a")]
 [assembly: System.Reflection.AssemblyProductAttribute("App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
